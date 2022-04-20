e2c627b9deb89b68cac4090e82ca17746c68d546
J00nasKalda <joonask@outlook.com>
2022-04-20 17:51:59 +0300

    fixed issues

--- src/views/CustomerInfoView.vue
+++ src/views/CustomerInfoView.vue
@@ -1,22 +1,43 @@
 <template>
+
   <div>
     <input placeholder="kliendi andmebaasi ID" v-model="customerId">
-
     <br>
     <br>
+    <table>
+      <tr>
+        <td>Veerg1</td>
+        <td>Veerg 2</td>
+      </tr>      <tr>
+        <td>rida 1</td>
+        <td>rida 1</td>
+      </tr>
+    </table>
 
     <button v-on:click="findCustomerById">Leia customer ID järgi</button>
-
     <br>
     <br>
+   eesnimi: {{customer.firstName}}
+    <br>
+    perekonnanimi: {{customer.lastName}}
+    <br>
+    isikukood: {{customer.isikukood}}
 
-    {{ customer }}
+<!--    {-->
+<!--    "id": 1,-->
+<!--    "firstName": "Otto",-->
+<!--    "lastName": "Triin",-->
+<!--    "isikukood": "40000000001"-->
+<!--    }-->
 
   </div>
+
+
 </template>
 
 <script>
 export default {
+
   name: 'CustomerInfo',
   data: function () {
     return {
@@ -34,23 +55,13 @@ export default {
       }).then(response => {
         this.customer = response.data
         console.log(response.data)
-      }).catch(error => console.log(error))
+
+
+      }).catch( error => console.log(error))
     }
 
 
-    // greeting: function () {
-    //   this.greetMessage = "Tere "
-    // },
-    // greetingWithName: function (firstname) {
-    //   console.log("Me oleme siin")
-    //   this.greeting()
-    //   this.firstname = firstname
-    //   this.greetMessage += this.firstname
-    // }
   }
 }
+
 </script>
-
-<style scoped>
-
-</style>
