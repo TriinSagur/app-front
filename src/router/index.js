4c105a51f9013ff33cc329001443d5b1edc74f4c
priitrs <priitrs@gmail.com>
2022-04-20 16:40:12 +0300

    PRIIT esimene vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CustomerInfoView from "@/views/CustomerInfoView";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'homeRoute',
     component: HomeView
   },
   {
+    path: '/customer',
+    name: 'customerRoute',
+    component: CustomerInfoView
+  },
+  {
     path: '/about',
-    name: 'about',
+    name: 'aboutRoute',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
---
+++ src/views/CustomerInfoView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <input placeholder="kliendi andmebaasi ID" v-model="customerId">
+
+    <br>
+
+<button v-on:click="findCustomerById" >Leia Customer Id järgi</button>
+    <br>
+    <br>
+  eesnimi:  {{customer.firstName}}
+    <br>
+    perekonnanimi:   {{customer.lastName}}
+    <br>
+    isikukood:   {{customer.isikukood}}
+    <br>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomerInfo",
+
+  data: function () {
+    return {
+      customerId: 0,
+      customer: {}
+
+    }
+  },
+
+  methods: {
+    findCustomerById: function () {
+      this.$http.get('/customer/id',{
+        params: {
+          id: this.customerId,
+        }
+      }).then(response => {
+        this.customer = response.data
+        console.log(response.data.firstName)
+      }).catch(error => console.log(error))
+    }
+
+  }
+
+}
+</script>
+
