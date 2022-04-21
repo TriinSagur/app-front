0a866cbca4b9ce261f86a78d58f51aed2da96182
Rain Tüür <rain.tuur@gmail.com>
2022-04-21 10:53:11 +0300

    MASTER fix

--- src/views/CustomerInfoView.vue
+++ src/views/CustomerInfoView.vue
@@ -2,33 +2,6 @@
 
   <div>
     <input placeholder="kliendi andmebaasi ID" v-model="customerId">
-    <br>
-    <br>
-    <table>
-      <tr>
-        <td>Veerg1</td>
-        <td>Veerg 2</td>
-      </tr>      <tr>
-        <td>rida 1</td>
-        <td>rida 1</td>
-      </tr>
-    </table>
-
-    <button v-on:click="findCustomerById">Leia customer ID järgi</button>
-    <br>
-    <br>
-   eesnimi: {{customer.firstName}}
-    <br>
-    perekonnanimi: {{customer.lastName}}
-    <br>
-    isikukood: {{customer.isikukood}}
-
-<!--    {-->
-<!--    "id": 1,-->
-<!--    "firstName": "Otto",-->
-<!--    "lastName": "Triin",-->
-<!--    "isikukood": "40000000001"-->
-<!--    }-->
 
   </div>
 
