ac52aa94795c2cdbe1990a86a6b1880779a77580
kyllilum <kyllilumiste@gmail.com>
2022-04-21 11:37:44 +0300

    front

--- src/views/CustomerInfoView.vue
+++ src/views/CustomerInfoView.vue
@@ -1,41 +1,64 @@
 <template>
   <div>
 
-    <input placeholder="nimi" v-model="firstName">
+    <input placeholder="kliendi andmebaasi ID" v-model="customerId">
     <br>
     <br>
+    <table>
+      <tr>
+        <td>Veerg1</td>
+        <td>Veerg 2</td>
+      </tr>      <tr>
+        <td>rida 1</td>
+        <td>rida 1</td>
+      </tr>
+    </table>
 
-
-
-<!--    <button v-on:click="greetingWithName('Otto Triin')">Tervituse nupp</button>-->
-    <button v-on:click="findCustomerById">Leia customer ID-ga 1</button>
+    <button v-on:click="findCustomerById">Leia customer ID järgi</button>
     <br>
     <br>
-    {{ greetMessage }}
+   eesnimi: {{customer.firstName}}
+    <br>
+    perekonnanimi: {{customer.lastName}}
+    <br>
+    isikukood: {{customer.isikukood}}
+
+<!--    {-->
+<!--    "id": 1,-->
+<!--    "firstName": "Otto",-->
+<!--    "lastName": "Triin",-->
+<!--    "isikukood": "40000000001"-->
+<!--    }-->
+
   </div>
+
+
 </template>
 
 
 <script>
 export default {
 
-  name: "CustomerInfo",
+  name: 'CustomerInfo',
   data: function () {
     return {
-      firstName: '',
-      greetMessage: ''
+      customerId: 0,
+      customer: {}
     }
   },
   methods: {
     findCustomerById: function () {
+
       this.$http.get('/customer/id', {
-        params:{
-          id: 1
+        params: {
+          id: this.customerId
         }
       }).then(response => {
-        console.log(response.data.firstName)
-      }).catch(error => console.log(error))
+        this.customer = response.data
+        console.log(response.data)
 
+
+      }).catch( error => console.log(error))
     }
 
 
