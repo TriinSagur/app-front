4f095682b6b6b5d01820846f3d3d4b3c0483d30f
ErgoTammel <ergotammel@gmail.com>
2022-04-21 16:18:09 +0300

    Läks minna

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
+      <router-link to="/new-customer">Add Customers</router-link> |
+      <router-link to="/customer">Customer info</router-link> |
+      <router-link to="/account-info">Account info</router-link> |
+      <router-link to="/input">Input</router-link> |
+      <router-link to="/output">Output</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
---
+++ src/views/InputPageView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+
+    <div>
+      <input type="text" placeholder="Eesnimi" v-model="customer.firstName">
+      <br>
+      <br>
+      <input type="text" placeholder="Perekonnanimi" v-model="customer.lastName">
+      <br>
+      <br>
+      <input type="text" placeholder="Isikukood" v-model="customer.isikukood">
+      <br>
+      <br>
+      <button v-on:click="saveDataToSessionStorage" type="button" class="btn btn-outline-danger">Session storage</button>
+      <button v-on:click="displayTableDiv" type="button" class="btn btn-outline-success m-3">Local storage</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputPageView",
+  data: function () {
+    return {
+      customer: {}
+    }
+  },
+  methods: {
+    saveDataToSessionStorage: function () {
+      sessionStorage.setItem('firstName', this.customer.firstName)
+      sessionStorage.setItem('lastName', this.customer.lastName)
+      sessionStorage.setItem('isikukood', this.customer.isikukood)
+      sessionStorage.setItem('customer', JSON.stringify(this.customer))
+    },
+    saveDataToLocalStorage: function () {
+      localStorage.setItem('firstName', this.customer.firstName)
+      localStorage.setItem('lastName', this.customer.lastName)
+      localStorage.setItem('isikukood', this.customer.isikukood)
+      localStorage.setItem('customer', JSON.stringify(this.customer))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
