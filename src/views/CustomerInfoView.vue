f8e966819ef1d40ba73aa3ac1a087e30420ef28b
Henri1988 <henri.eessalu@gmail.com>
2022-04-21 09:25:00 +0300

    Commit

---
+++ src/views/CustomerInfoView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+
+    <input placeholder="nimi"  v-model="firstName">
+    <br>
+    <br>
+
+    <button v-on:click="findCustomerById">Leia customer ID'ga 1</button>
+
+    <br>
+    <br>
+    {{ greetMessage }}
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: "CustomerInfo",
+  data: function () {
+    return {
+      firstName: "",
+      greetMessage: ""
+    }
+  },
+  methods: {
+    findCustomerById: function(){
+
+      this.$http.get('/customer/id',{
+        params: {
+          id:1
+        }
+      }) .then(response => {
+        console.log(response.data)
+      }).catch(error=> console.log(error))
+    }
+
+
+
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
