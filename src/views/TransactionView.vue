7b64d6edea41c02e28ef40ef260dcc102603dc49
holgerpart <Hobevalge1>
2022-04-22 14:59:53 +0300

    HOLGER update

--- src/views/TransactionView.vue
+++ src/views/TransactionView.vue
@@ -5,15 +5,35 @@
     <section>
       <h3>vali konto</h3>
       <ul class="list-group">
-        <li v-on:click="" class="list-group-item" v-for="account in accounts"><input type="radio" v-model="accountId" :value= "account.accountId" >{{account.accountNumber}}  {{account.balance}}</li>
-
+        <li class="list-group-item" v-for="account in accounts">
+          <input type="radio" v-model="accountId" :value="account.accountId">{{ account.accountNumber }}
+          €{{ account.balance }}
+        </li>
       </ul>
-
-      <br />
-      <span>value: {{ accountId }}</span>
     </section>
+    <button v-on:click="getStatementByAccountId" type="button" class="btn btn-success">Näita</button>
+    <div v-if="statements.length > 0 && initialClick">
+      <table>
+        <tr>
+          <th>Saatja</th>
+          <th>Saaja</th>
+          <th>Summa</th>
+          <th>Jääk</th>
+          <th>Aeg</th>
+        </tr>
+        <tr v-for="statement in statements">
+          <td>{{ statement.senderAccountNumber }}</td>
+          <td>{{ statement.receiverAccountNumber }}</td>
+          <td>{{ statement.amount }}</td>
+          <td>{{ statement.balance }}</td>
+          <td>{{ statement.transactionDateTime }}</td>
+        </tr>
+      </table>
 
-
+    </div>
+    <div v-else-if="initialClick">
+      Ei leidnud tulemusi
+    </div>
   </div>
 </template>
 
@@ -27,7 +47,9 @@ export default {
     return {
       accounts: {},
       customerId: this.$route.query.id,
-      accountId: null
+      accountId: null,
+      statements: {},
+      initialClick: false
     }
   }, methods: {
     findAccountsInfoByCustomerId: function (id) {
@@ -39,20 +61,29 @@ export default {
       })
           .then(response => {
             this.accounts = response.data
+            this.accountId = this.accounts[0].accountId
           })
           .catch(error => console.log(error.response.data))
+    },
+    getStatementByAccountId: function () {
+      this.$http.get("/statement/account-id", {
+        params: {
+          accountId: this.accountId
+
+        }
+      }
+      )   .then(response => {
+            this.initialClick = true
+            this.statements = response.data.statements
+            console.log(response.data)
+          }).catch(error => {
+            console.log(error)
+          })
     }
   },
-  getStatementByByAccountId: function (){
-    this.$http.get("/statement/account-id"),{
-      params: {
-        accountId:1
-      }
-    }
-  }
-
   mounted() {
     this.findAccountsInfoByCustomerId(1)
+
   }
 }
 </script>
