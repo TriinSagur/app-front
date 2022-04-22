fce3abfa822baaca516b81f5135f6621865fb032
sergeijagur <sergei.jagur@gmail.com>
2022-04-22 14:38:29 +0300

    SERGEI update

--- src/views/TransactionView.vue
+++ src/views/TransactionView.vue
@@ -5,20 +5,43 @@
     <br>
 
 
-    <section>
-      <h3>Vali konto</h3>
+    <div>
+      <section>
+        <h3>Vali konto</h3>
+        <ul class="list-group">
+          <li class="list-group-item" v-for="account in accounts"><input type="radio" v-model="accountId"
+                                                                         :value="account.accountId">
+            {{ account.accountNumber }} EUR {{ account.balance }}
+          </li>
+        </ul>
+      </section>
 
-    <ul class="list-group">
-      <li class="list-group-item" v-for="account in accounts"> <input type="radio" v-model="accountId" :value="account.accountId">{{ account.accountNumber}} EUR {{account.balance}}</li>
+      <button v-on:click="getStatementByAccountId" type="button" class="btn btn-success m-5">Naita</button>
 
-    </ul>
+      <div v-if="statements.length > 0">
+      <table>
+        <tr>
+          <th>Saatja</th>
+          <th>Saaja</th>
+          <th>Summa</th>
+          <th>Kontojaak</th>
+          <th>Aeg</th>
+        </tr>
+        <tr v-for="statement in statements">
+          <td>{{ statement.senderAccountNumber }}</td>
+          <td>{{ statement.receiverAccountNumber }}</td>
+          <td>{{ statement.amount }}</td>
+          <td>{{ statement.balance }}</td>
+          <td>{{ statement.transactionDateTime }}</td>
+        </tr>
+      </table>
+      </div>
 
-    <br />
-    <span>choosen account ID: {{ accountId }}</span>
-    </section>
+      <div v-else-if="initialClick">
+        Ei leidnud tulemusi
+      </div>
 
-
-
+    </div>
   </div>
 </template>
 
@@ -33,25 +56,44 @@ export default {
     return {
       accounts: {},
       customerId: this.$route.query.id,
-      accountId: null
+      accountId: null,
+      statements: {},
+      initialClick: false
     }
   },
   methods: {
-    findAccountsInfoByCustomerId: function (id) {
+    findAccountsInfoByCustomerId: function (customerId) {
 
       this.$http.get('/account/customer-id', {
         params: {
-          id: id
+          id: customerId
         }
       })
           .then(response => {
             this.accounts = response.data
+            this.accountId = this.accounts[0].accountId
           })
           .catch(error => console.log(error.response.data))
     },
+
+    getStatementByAccountId: function (id) {
+      this.$http.get("/statement/account-id", {
+            params: {
+              accountId: this.accountId
+            }
+          }
+      ).then(response => {
+        this.initialClick = true;
+        this.statements = response.data.statements
+      }).catch(error => {
+        console.log(error)
+      })
+    }
   },
   mounted() {
     this.findAccountsInfoByCustomerId(1);
+
+
   }
 }
 </script>
