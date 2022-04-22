8768c9118997107e2ef4952375057da87780b4ff
500 <git.hub@myrk.eu>
2022-04-22 09:52:06 +0300

    JAAN update

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,6 @@ import HomeView from '../views/HomeView.vue'
 import CustomerInfoView from "@/views/CustomerInfoView";
 import NewCustomerView from "@/views/NewCustomerView";
 import AccountInfoView from "@/views/AccountInfoView";
-import OutputPageView from "@/views/OutputPageView";
-import InputPageView from "@/views/InputPageView";
 
 Vue.use(VueRouter)
 
@@ -29,16 +27,6 @@ const routes = [
         path: '/account-info',
         name: 'accountRoute',
         component: AccountInfoView
-    },
-    {
-        path: '/input',
-        name: 'inputRoute',
-        component: InputPageView
-    },
-    {
-        path: '/output',
-        name: 'outputRoute',
-        component: OutputPageView
     },
     {
         path: '/about',
