910d412e8cedfe1fdd03b3fbd61854608b117603
KarliEST <karlikullasepp@gmail.com>
2022-04-25 15:05:30 +0300

    Bank: front-endi tegemine

--- src/views/NewCustomerView.vue
+++ src/views/NewCustomerView.vue
@@ -4,8 +4,20 @@
     <input type="text" placeholder="eesnimi" v-model="customer.firstName" style="margin: 10px">
     <input type="text" placeholder="perekonnanimi" v-model="customer.lastName" style="margin: 10px">
     <input type="text" placeholder="isikukood" v-model="customer.isikukood" style="margin: 10px">
-    <button type="submit" v-on:click="addNewCustomer" style="margin: 10px">Lisa uus klient</button>
 
+
+    <input type="file" @change="handleImage" accept="image/x-png,image/jpeg">
+
+    <br>
+    <br>
+    <button type="submit" class="btn btn-outline-success m-3" v-on:click="addNewCustomer">Lisa uus klient</button>
+    <button v-on:click="addNewCustomerPicture" type="button" class="btn btn-outline-success m-3">Lisa pilt</button>
+    <button v-on:click="getAllPictures" type="button" class="btn btn-outline-success m-3">Kuva pilte</button>
+
+    <div v-for="picture in pictures" class="img-fluid">
+      <label>{{picture.title}}</label>
+      <img :src="picture.data">
+    </div>
 
   </div>
 </template>
@@ -15,10 +27,32 @@ export default {
   name: 'NewCustomerView',
   data: function () {
     return {
-    customer:{}
+      customer: {},
+      pictureExport: {},
+      pictures: {}
     }
   },
   methods: {
+
+    handleImage(event) {
+      // this.displayInputPicture = true
+      // this.displayUploadPictureDetailsOptions = true
+
+      const selectedImage = event.target.files[0];
+      this.createBase64Image(selectedImage);
+    },
+
+    createBase64Image(fileObject) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.pictureExport.data = reader.result;
+        console.log(this.pictureExport.data)
+      };
+      reader.onerror = function (error) {
+        alert(error);
+      }
+      reader.readAsDataURL(fileObject);
+    },
 
     addNewCustomer: function () {
       this.$http.post('/customer', this.customer)
@@ -28,12 +62,34 @@ export default {
           })
           .catch(error => {
             alert(error.response.data.detail)
+            console.log(error.response.data)
           })
+    },
+    addNewCustomerPicture: function () {
+
+      this.$http.post("/picture/in", this.pictureExport
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    getAllPictures: function () {
+      this.$http.get("/picture/all")
+          .then(response => {
+            this.pictures = response.data
+            console.log(response.data)
+          }).catch(error => {
+        console.log(error)
+      })
     }
+
   }
 }
 </script>
 
 <style scoped>
-
+img {
+  width: 80%;
+}
 </style>
--- src/views/TransactionView.vue
+++ src/views/TransactionView.vue
@@ -1,73 +1,161 @@
 <template>
   <div>
     <h1>Pangatoimingud</h1>
-    <div>
 
-      <!--      ACCOUNTS    -->
-      <section>
-        <h3>vali konto</h3>
-        <ul class="list-group">
-          <li class="list-group-item" v-for="account in accounts">
-            <input  type="radio" v-model="accountId"
-                   :value="account.accountId">{{ account.accountNumber }} -
-            €{{ account.balance }}
-          </li>
-        </ul>
-      </section>
+    <div v-if="successMessage.length > 0" class="alert alert-success" role="alert">
+      {{ successMessage }}
+    </div>
 
-      <button type="button" class="btn btn-primary" v-on:click="getStatementByAccountId" style="margin: 10px">Kuva konto
-        väljavõtet
+    <div v-if="accountActionsDiv">
+      <div class="d-inline-flex p-2">
+        <section>
+          <h3>vali konto</h3>
+          <ul class="list-group">
+            <li class="list-group-item" v-for="account in accounts">
+              <input type="radio" v-model="accountId" :value="account.accountId">{{ account.accountNumber }}
+              €{{ account.balance }}
+            </li>
+          </ul>
+        </section>
+      </div>
+
+      <br>
+      <button v-on:click="getStatementByAccountId" type="button" class="btn btn-outline-success m-3">
+        Kuva kontoväljavõtet
       </button>
+      <br>
+      <button v-on:click="startNewPayment" type="button" class="btn btn-outline-success m-3">
+        Alusta ülekannet
+      </button>
+      <br>
+    </div>
 
-      <!--      STATEMENTS TABEL      -->
-      <div v-if="statements.length > 0">
-        <table>
-          <tr>
-            <th>Saatja</th>
-            <th>Saaja</th>
-            <th>Summa</th>
-            <th>Jääk</th>
-            <th>Aeg</th>
-          </tr>
-          <tr v-for="statement in statements">
-            <td>{{ statement.senderAccountNumber }}</td>
-            <td>{{ statement.receiverAccountNumber }}</td>
-            <td>{{ statement.amount }}</td>
-            <td>{{ statement.balance }}</td>
-            <td>{{ statement.transactionDateTime }}</td>
-          </tr>
-        </table>
-      </div>
-      <div v-else-if="initialClick">
-        Ei leidnud tulemust
-      </div>
-      <div v-else>
 
+    <div v-if="transferActionsDiv">
+      <div>
+        <div class="d-inline-flex p-2">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect01">Saatja Nimi</label>
+            </div>
+            <input type="text" v-model="senderName">
+          </div>
+        </div>
       </div>
+
+      <!--  SAATJA    -->
+      <div>
+        <div class="d-inline-flex p-2">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect01">Saatja konto</label>
+            </div>
+            <select class="custom-select" id="inputGroupSelect01" v-model="accountId">
+              <option selected v-for="account in accounts" :value="account.accountId">{{
+                  account.accountNumber
+                }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+
+      <!--  SAAJA KONTO NUMBER    -->
+      <div>
+        <div class="d-inline-flex p-2">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect01">Saaja konto</label>
+            </div>
+            <input type="text" v-model="receiverAccountNumber">
+          </div>
+        </div>
+      </div>
+
+      <!--  SUMMA    -->
+      <div>
+        <div class="d-inline-flex p-2">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect01">Summa</label>
+            </div>
+            <input type="number" placeholder="0" v-model="amount">
+          </div>
+        </div>
+      </div>
+      <button v-on:click="backToAccountActions" type="button" class="btn btn-outline-success m-3">
+        Tagasi
+      </button>
+      <br>
+      <button v-on:click="sendMoney" type="button" class="btn btn-outline-success m-3">
+        Teosta makse
+      </button>
+      <br>
 
     </div>
 
+
+    <!--    KONTO VÄLJAVÕTE   -->
+    <div v-if="statementDiv">
+      <button v-on:click="backToAccountActions" type="button" class="btn btn-outline-success m-3">
+        Tagasi
+      </button>
+      <StatementTable :initial-click="initialClick" :statements="statements"/>
+    </div>
+
+
   </div>
+
+
 </template>
 
 <script>
-import AccountInfoTable from "@/components/AccountInfoTable";
+import StatementTable from "@/components/StatementTable";
 
 export default {
   name: 'TransactionView',
-  components: {AccountInfoTable},
+  components: {StatementTable},
+
   data: function () {
     return {
       accounts: {},
+      firstName: 'Otto',
+      lastName: 'Triin',
       customerId: this.$route.query.id,
       accountId: null,
       statement: {},
       statements: {},
-      initialClick: false
+      initialClick: false,
+      selectedAccountId: null,
+      receiverAccountNumber: '',
+      senderName: 'Otto Triin',
+      senderAccountId: null,
+      amount: null,
+      accountActionsDiv: true,
+      transferActionsDiv: false,
+      statementDiv: false,
+      successMessage: ''
 
-    }
+    };
   },
   methods: {
+
+    hideAllDivs: function () {
+      this.successMessage = ''
+      this.accountActionsDiv = false
+      this.transferActionsDiv = false
+      this.statementDiv = false
+    },
+
+    backToAccountActions: function () {
+      this.hideAllDivs()
+      this.accountActionsDiv = true
+      this.receiverAccountNumber = null
+      this.amount = null
+    },
+
+
     findAccountsInfoByCustomerId: function (id) {
       this.$http.get('/account/customer-id', {
         params: {
@@ -77,8 +165,26 @@ export default {
           .then(response => {
             this.accounts = response.data
             this.accountId = this.accounts[0].accountId
+            this.selectedAccountId = this.accountId
           })
           .catch(error => console.log(error.response.data))
+    },
+
+    sendMoney: function () {
+      let transferRequest = {
+        senderAccountId: this.accountId,
+        receiverAccountNumber: this.receiverAccountNumber,
+        amount: this.amount
+      }
+      this.$http.post("/bank/send", transferRequest
+      ).then(response => {
+        this.hideAllDivs()
+        this.successMessage = 'Ülekanne õnnestus, tehingu ID: ' + response.data.referenceNumber
+        this.accountActionsDiv = true
+
+      }).catch(error => {
+        console.log(error)
+      })
     },
 
     getStatementByAccountId: function () {
@@ -88,12 +194,20 @@ export default {
         }
       })
           .then(response => {
+            this.hideAllDivs()
+            this.statementDiv = true
             this.statements = response.data.statements
             this.initialClick = true
             console.log(response.data)
           })
           .catch(error => console.log(error))
+    },
+    startNewPayment: function () {
+      this.hideAllDivs()
+      this.transferActionsDiv = true
+      this.selectedAccountId = this.accountId
     }
+
   },
 
   mounted() {
@@ -102,6 +216,3 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
