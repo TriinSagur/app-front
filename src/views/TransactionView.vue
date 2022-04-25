6b0b126658ea98bf80a13146de331678f2eef62b
ReimoM <reimosalo@gmail.com>
2022-04-25 11:32:32 +0300

    REIMO UPDATE

--- src/views/TransactionView.vue
+++ src/views/TransactionView.vue
@@ -2,40 +2,111 @@
   <div>
     <h1>Pangatoimingud</h1>
 
-    <div>
+    <div v-if="accountActionsDiv">
 
       <div class="d-inline-flex p-2">
-      <section>
-        <h3>vali konto</h3>
-        <ul class="list-group">
-          <li class="list-group-item" v-for="account in accounts">
-            <input type="radio" v-model="accountId" :value="account.accountId">{{ account.accountNumber }}
-            {{ '€' + account.balance }}
-          </li>
-        </ul>
-      </section>
-        <br>
-        <br>
+        <section>
+          <h3>vali konto</h3>
+          <ul class="list-group">
+            <li class="list-group-item" v-for="account in accounts">
+              <input type="radio" v-model="accountId" :value="account.accountId">{{ account.accountNumber }}
+              €{{ account.balance }}
+            </li>
+          </ul>
+        </section>
       </div>
+
+      <br>
+      <button v-on:click="getStatementByAccountId" type="button" class="btn btn-outline-success m-3">
+        Kuva kontoväljavõtet
+      </button>
+
+      <button v-on:click="startNewPayment" type="button" class="btn btn-outline-success m-3">
+        Alusta ülekannet
+      </button>
+      <br>
+      <br>
+    </div>
+
+    <div v-if="transferActionsDiv">
+
+      <!--  SAAJA NIMI  -->
+      <div>
+        <div class="d-inline-flex p-2">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect01">Saatja Nimi</label>
+            </div>
+            <input type="text" v-model="senderName">
+          </div>
+        </div>
+      </div>
+
+      <!--  SAATJA    -->
+      <div>
+        <div class="d-inline-flex p-2">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect01">Saatja konto</label>
+            </div>
+            <select class="custom-select" id="inputGroupSelect01" v-model="accountId">
+              <option selected v-for="account in accounts" :value="account.accountId">{{
+                  account.accountNumber
+                }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <!--  SAAJA KONTO NUMBER    -->
+      <div>
+        <div class="d-inline-flex p-2">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect01">Saaja konto</label>
+            </div>
+            <input type="text" v-model="receiverAccountNumber">
+          </div>
+        </div>
+      </div>
+
+      <!--  Summa    -->
+      <div>
+        <div class="d-inline-flex p-2">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect01">Summa</label>
+            </div>
+            <input type="number" placeholder="0" v-model="amount">
+          </div>
+        </div>
+      </div>
+
+      <br>
+      <button v-on:click="backToAccountActions" type="button" class="btn btn-outline-success m-3">
+        Tagasi
+      </button>
+
+      <button v-on:click="startNewPayment" type="button" class="btn btn-outline-success m-3">
+        Teosta makse
+      </button>
       <br>
 
-      <button v-on:click="getStatementByAccountId" type="button" class="btn btn-success m-3">Kuva kontoväljavõte</button>
-      <button v-on:click="startNewPayment" type="button" class="btn btn-success m-3">Alusta ülekannet</button>
+    </div>
 
-      <div>
-        <h5>{{firstName}} {{lastName}}</h5>
-
-        <select v-model="accountId">
-          <option v-for="account in accounts" :value="account.accountId">{{account.accountNumber}}</option>
-        </select>
-      </div>
-
+<!--    konto väljavõte-->
+    <div v-if="statementDiv">
+      <button v-on:click="backToAccountActions" type="button" class="btn btn-outline-success m-3">
+        Tagasi
+      </button>
       <StatementTable :initial-click="initialClick" :statements="statements"/>
-
     </div>
 
 
   </div>
+
+
 </template>
 
 <script>
@@ -44,20 +115,42 @@ import StatementTable from "@/components/StatementTable";
 export default {
   name: 'TransactionView',
   components: {StatementTable},
+
   data: function () {
     return {
       accounts: {},
+      firstName: 'Otto',
+      lastName: 'Triin',
+      selectedAccountId: null,
       customerId: this.$route.query.id,
       accountId: null,
       statements: {},
       initialClick: false,
-      firstName: 'Otto',
-      lastName: 'Triin',
-      selectedAccountId: null,
+      receiverAccountNumber: '',
+      senderName: 'Otto Triin',
+      amount: null,
+      accountActionsDiv: true,
+      transferActionsDiv: false, // kui ei taha lehe avamisel näha(peidab kindla div osa ära)
+      statementDiv: false,
+
     }
   },
 
   methods: {
+    hideAllDivs: function () {
+      this.accountActionsDiv = false
+      this.transferActionsDiv = false
+      this.statementDiv = false
+    },
+    backToAccountActions: function () {
+      this.hideAllDivs()
+      this.accountActionsDiv = true
+      this.receiverAccountNumber = null
+      this.amount = null
+
+    }
+
+    ,
     findAccountsInfoByCustomerId: function (id) {
       this.$http.get('/account/customer-id', {
         params: {
@@ -78,6 +171,8 @@ export default {
             }
           }
       ).then(response => {
+        this.hideAllDivs()
+        this.statementDiv = true
         this.initialClick = true
         this.statements = response.data.statements
         console.log(response.data)
@@ -86,9 +181,9 @@ export default {
       })
     },
     startNewPayment: function () {
-      console.log('selectedAccountId = ' + selectedAccountId)
-
-
+      this.hideAllDivs()
+      this.transferActionsDiv = true
+      this.selectedAccountId = this.accountId
     }
   },
 
