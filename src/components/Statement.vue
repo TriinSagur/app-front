336f2bd87b91268e892c657302c2228ac5dd4688
priitrs <priitrs@gmail.com>
2022-04-25 15:04:20 +0300

    PRIIT update

--- src/components/Statement.vue
+++ src/components/Statement.vue
@@ -1,25 +1,29 @@
 <template>
-  <div v-if="statements.length > 0">
-    <table>
-      <tr>
-        <th>Saatja</th>
-        <th>Saaja</th>
-        <th>Summa</th>
-        <th>Jääk</th>
-        <th>Aeg</th>
-      </tr>
-      <tr v-for="statement in statements">
-        <td>{{ statement.senderAccountNumber }}</td>
-        <td>{{ statement.receiverAccountNumber }}</td>
-        <td>{{ statement.amount }}</td>
-        <td>{{ statement.balance }}</td>
-        <td>{{ statement.transactionDateTime }}</td>
-      </tr>
-    </table>
+
+  <div class="d-inline-flex p2">
+    <div v-if="statements.length > 0">
+      <table class="table table-hover">
+        <tr>
+          <th>Saatja</th>
+          <th>Saaja</th>
+          <th>Summa</th>
+          <th>Jääk</th>
+          <th>Aeg</th>
+        </tr>
+        <tr v-for="statement in statements">
+          <td>{{ statement.senderAccountNumber }}</td>
+          <td>{{ statement.receiverAccountNumber }}</td>
+          <td>{{ statement.amount }}</td>
+          <td>{{ statement.balance }}</td>
+          <td>{{ statement.transactionDateTime }}</td>
+        </tr>
+      </table>
+    </div>
+    <div v-else-if="initialClick">
+      Ei leidnud tulemust!
+    </div>
   </div>
-  <div v-else-if="initialClick">
-    Ei leidnud tulemust!
-  </div>
+
 </template>
 <script>
 export default {
